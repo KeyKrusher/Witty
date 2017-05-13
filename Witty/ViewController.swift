2fcac4d8c6cb04e0b1708652d922003e189a41f3
Jack Clark <chubz@Jacks-Mac-mini.local>
2017-05-13 17:06:55 -0500

    Initial Commit

---
+++ Witty/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Witty
+//
+//  Created by Jack Clark on 5/13/17.
+//  Copyright © 2017 Jack Clark. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
